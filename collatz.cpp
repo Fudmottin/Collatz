05f75175f4af6405fabead6df1236f136c4f89b5
Fudmottin <fudmottin@gmail.com>
2024-04-12 18:44:46 -0400

    No factorization. Keep track of odds and evens.

--- collatz.cpp
+++ collatz.cpp
@@ -9,99 +9,9 @@
 
 using boost::multiprecision::cpp_int;
 
-const int MILLERRABINITERATIONS = 40;
-
-cpp_int powMod(cpp_int base, cpp_int exp, const cpp_int& mod) {
-    cpp_int result = 1;
-    base %= mod;
-    while (exp > 0) {
-        if (exp & 1) {
-            result = (result * base) % mod;
-        }
-        base = (base * base) % mod;
-        exp >>= 1;
-    }
-    return result;
-}
-
-bool millerRabinTest(const cpp_int& n) {
-    if (n <= 1 || n == 4) return false;
-    if (n <= 5) return true;
-
-    cpp_int d = n - 1;
-    while ((d & 1) == 0) {
-        d >>= 1;
-    }
-
-    boost::random::mt19937_64 rng(std::random_device{}());
-    for (int i = 0; i < MILLERRABINITERATIONS; i++) {
-        cpp_int a = boost::random::uniform_int_distribution<cpp_int>(2, n - 2)(rng);
-        cpp_int x = powMod(a, d, n);
-        if (x == 1 || x == n - 1) continue;
-
-        bool isPrime = false;
-        for (cpp_int r = 1; r < d; r *= 2) {
-            x = powMod(x, 2, n);
-            if (x == 1) return false;
-            if (x == n - 1) {
-                isPrime = true;
-                break;
-            }
-        }
-
-        if (!isPrime) return false;
-    }
-
-    return true;
-}
-
-cpp_int findFactor(const cpp_int& n) {
-    if ((n & 1) == 0)
-        return cpp_int(2);
-    if (millerRabinTest(n))
-        return n;
-    for (cpp_int factor = 3; factor * factor <= n; factor += 2)
-        if (n % factor == 0)
-            return factor;
-    return n;
-}
-
-void primeFactorization(cpp_int n) {
-    cpp_int fac;
-    do {
-        fac = findFactor(n);
-        cpp_int exp = 0;
-
-        if (fac == n) {
-            std::cout << n;
-            return;
-        }
-
-        do {
-            ++exp;
-            n /= fac;
-        } while ((n % fac) == 0);
-
-        std::cout << fac;
-
-        if (exp > 1) {
-            std::cout << "^";
-            if (!millerRabinTest(exp)) {
-                std::cout << "(";
-                primeFactorization(exp);
-                std::cout << ")";
-            } else std::cout << exp;
-        }
-
-        if (n > 2) std::cout << " * ";
-        std::cout.flush();
-    } while (n > 1);
-}
-
-
 inline cpp_int collatz(const cpp_int& n) {
     if ((n & 1) == 0)
-        return n / 2;
+        return n >> 1;
     else
         return n * 3 + 1;
 }
@@ -170,38 +80,31 @@ int main (int argc, char *argv[]) {
             boost::random::uniform_int_distribution<cpp_int>
             (4, max_seed)(rng);
 
-            // Load the dice. We will make the seed evenly divisible
-            // by 3 but not by 2. This should result in a sequence that
-            // only the seed is divisible by 3. 3n+1 can't be divisible
-            // by 3. We will leave any powers of 2 remain and see what
-            // happens. When the collatz function hits a power of 2, it
-            // should dive straight to 1.
-        
-            seed += 3 - seed % 3; // Yeah, that's all we have to do.
+            // Load the dice. 3n+1 will always produce a number that is
+            // not divisible by 3. We will simply make sure that the seeed
+            // is also not divisible by 3.
+            if (seed % 3 == 0) ++seed; // Yeah, that's all we have to do.
 
             auto seq = runCollatz(seed);
+            long long odds = 0;
+            long long evens = 0;
 
-            std::cout << "collatz:\n";
-            for (auto n : seq) {
-                std::cout << n << "\t\t\t\t\t=\t";
-                primeFactorization(n);
-                std::cout << "\n";
-            }
+            for (auto n : seq)
+                if ((n & 1) == 0) ++evens; else ++odds;
+
+            std::cout << "collatz: " << evens << "/" << odds << "\n";
+            for (auto n : seq)
+                std::cout << n << "\n";
             std::cout << std::endl;
 
-            if (!prev_seq.empty()) {
-                auto node = findNode(prev_seq, seq);
-                nodes.insert(node);
-            }
+            if (!prev_seq.empty())
+                nodes.insert(findNode(prev_seq, seq));
             prev_seq = seq;
         }
 
         std::cout << "nodes:\n";
-        for (auto n : nodes) {
-            std::cout << n << "\t\t\t\t\t=\t";
-            primeFactorization(n);
-            std::cout << "\n";
-        }
+        for (auto n : nodes)
+            std::cout << n << "\n";
     }
     catch (const std::exception& e) {
         std::cerr << "Exception thrown: " << e.what() << std::endl;
