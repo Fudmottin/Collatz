3935429602fdcd8aab789f1ced4d44875950e172
Fudmottin <fudmottin@gmail.com>
2024-06-16 18:43:54 -0400

    back to counting steps

--- collatz3.cpp
+++ collatz3.cpp
@@ -1,21 +1,23 @@
 #include <iostream>
 
-inline void collatz(uint64_t seed, uint64_t prev_seed) {
-    while ((seed > 1) && (seed > prev_seed)) {
+inline uint64_t collatz(uint64_t seed) {
+    uint64_t steps = 0;
+    while (seed > 1) {
         while ((seed & 1) == 0) {
+            ++steps;
             seed >>= 1;
         }
         if (seed > 1) {
+            ++steps;
             seed = seed * 3 + 1;
         }
     }
+    return steps;
 }
 
 int main () {
-    for(uint64_t seed = 1; seed < 1'000'000'001; ++seed) {
-        collatz(seed, seed - 1);
-        std::cout << "Seed: " << seed << "\r";
-    }
+    for(uint64_t seed = 1; seed < 1'000'000'001; ++seed)
+        std::cout << "Seed: " << seed << " Steps: " << collatz(seed) << "\r";
     
     std::cout << std::endl;
 
